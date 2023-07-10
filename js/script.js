d23869e2e94f7f99418493f627e04d5c74d7d2cc
whoxer <natanaelvieira1@hotmail.com>
2023-07-10 03:07:36 -0300

    código javascrispt deletado; ainda pensando sobre o que farei com o js aqui

--- js/script.js
+++ js/script.js
@@ -1,13 +1,1 @@
-function show_header() {
-    var hr = "<hr>";
-    var w = "<h1>whoxer</h1>"
-    var header_o = "<header>";
-    var header_c = "</header>";
-    var home = "<li><a href='../index.html'>| home |</a></li>";
-    var mstd = "<li><a href='https://mastodon.social/@wh0x3r'>| mastodon |</a></li>";
-    var gith = "<li><a href='https://github.com/whoxer'>| github |</a></li>";
-
-    document.getElementById('header').innerHTML += hr + header_o + w +
-    "<nav>" + "<ul class='cabeçalho'>" + home + mstd + gith + "</ul>" +
-    "</nav>" + header_c + hr;
-}
+// code
